0bc1751eadaf97790a0493f48a9703c0ee39df13
heartless-unicorn <pavlovamofficial@gmail.com>
2022-09-13 23:17:24 +0200

    added extentions
    Axios and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return <div className="App"></div>;
